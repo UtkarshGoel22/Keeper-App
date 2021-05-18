84896cc0c971fcbc13fd7857dc112ba8dbbe23e9
UtkarshGoel22 <utkarsh.goel22@gmail.com>
2021-05-18 20:17:27 +0530

    Delete note.js file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
-import notes from '../notes';
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map((note) => <Note key={note.key} title={note.title} content={note.content} />)}
+            <Note key={1} title="Note Title" content="Note Content"/>
             <Footer />
         </div>
     );
